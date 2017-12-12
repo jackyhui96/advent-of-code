0c41a1a16b1345f0019e438f5245d2ca4b84192c
Jacky Hui <halojax@hotmail.com>
2017-12-12 15:45:38 +0000

    Added solution for day 12 of AOC.

---
+++ aoc_12_2017.erl
@@ -0,0 +1,50 @@
+-module(aoc_12_2017).
+-compile(export_all).
+
+main() ->
+    Input = input(),
+    Part1 = length(find_programs_in_group(Input, <<"0">>, [])),
+    ct:pal("Part1: ~p~n", [Part1]),
+    Part2 = length(find_all_groups(Input)),
+    ct:pal("Part2: ~p~n", [Part2]).
+
+
+find_all_groups(Map) ->
+    Programs = maps:keys(Map),
+    find_all_groups(Programs, Map, []).
+
+find_all_groups([], _, Acc) -> Acc;
+find_all_groups([Program|Rest], Map, Acc) ->
+    Group = find_programs_in_group(Map, Program, []),
+    find_all_groups(Rest -- Group, Map, [Group|Acc]).
+
+
+find_programs_in_group(_Map, [], Acc) -> Acc;
+find_programs_in_group(Map, [Program|Rest], Acc) ->
+    NewAcc = case lists:member(Program, Acc) of
+        false ->
+            UnseenPrograms = find_programs_in_group(Map, Program, Acc),
+            [UP || UP <- UnseenPrograms, not lists:member(UP, Acc)] ++ Acc;
+        true ->
+            Acc
+    end,
+    find_programs_in_group(Map, Rest, NewAcc);
+    
+find_programs_in_group(Map, Program, Acc) ->
+    #{Program := ConnectedPrograms} = Map,
+    NewAcc = [Program|Acc],
+    UnseenPrograms = [CP || CP <- ConnectedPrograms, not lists:member(CP, NewAcc)],
+    case UnseenPrograms of
+        [] -> [Program];
+        _ ->
+            find_programs_in_group(Map, UnseenPrograms, NewAcc)
+    end.
+
+
+input() ->
+    {ok, Data} = file:read_file("day12_data.txt"),
+    Lines = binary:split(Data, <<"\n">>, [global, trim_all]),
+    maps:from_list([begin 
+        [Program|ConnectedPrograms] = binary:split(L, [<<" <-> ">>, <<", ">>], [global, trim_all]), 
+        {Program, ConnectedPrograms}
+     end|| L <- Lines]).
