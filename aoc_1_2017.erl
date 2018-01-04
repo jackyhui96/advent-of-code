e1d2797cc344abc6bddd2d9a5a70e25e78c8a8e1
Jacky Hui <halojax@hotmail.com>
2018-01-04 16:23:35 +0000

    Small improvements to day 1 for better readability.

--- aoc_1_2017.erl
+++ aoc_1_2017.erl
@@ -2,32 +2,30 @@
 -compile(export_all).
 
 main() ->
-    Input = input() ++ [(hd(input()))],
-    Input2 = input(),
-    Part1 = calc_captcha(Input, 0),
+    Input = input(),
+    Length = length(Input),
+
+    Part1 = calc_captcha(Input, 1),
     ct:pal("Part1: ~p~n", [Part1]),
-    Part2 = calc_captcha2(Input2, 1, length(Input2), 0),
+
+    Part2 = calc_captcha(Input, round(Length/2)),
     ct:pal("Part2: ~p~n", [Part2]).
 
-calc_captcha([], Acc) ->
-    Acc;
-calc_captcha([X,X|T], Acc) ->
-    calc_captcha([X|T], Acc+X);
-calc_captcha([_|T], Acc) ->
-    calc_captcha(T, Acc).
+calc_captcha(List, Offset) ->
+    Length = length(List),
+    calc_captcha(List, 1, Offset, Length, 0).
 
-calc_captcha2(_, Index, Length, Acc) when Index > Length ->
-    Acc;
-calc_captcha2(Input, Index, Length, Acc) ->
-    Val = lists:nth(Index, Input), 
-    TargetIndex = (Index - 1 + round(Length/2)) rem Length + 1,
-    NewAcc = case Val =:= lists:nth(TargetIndex, Input) of
-        true ->
+calc_captcha(_, Index, _, Length, Acc) when Index > Length -> Acc;
+calc_captcha(List, Index, Offset, Length, Acc) ->
+    Val = lists:nth(Index, List), 
+    TargetIndex = (Index - 1 + Offset) rem Length + 1,
+    NewAcc = case lists:nth(TargetIndex, List) of
+        Val -> 
             Acc + Val;
-        false ->
+        _ -> 
             Acc
     end,
-    calc_captcha2(Input, Index+1, Length, NewAcc).
+    calc_captcha(List, Index+1, Offset, Length, NewAcc).
 
 input() ->
     {ok, Data} = file:read_file("day1_data.txt"),
