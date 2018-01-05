55474f15e4366f1fc62806493e0d4df6e82ce267
Jacky Hui <halojax@hotmail.com>
2018-01-05 14:04:16 +0000

    Made day 16 faster by checking for a cycle and using that to work out the answer.

--- aoc_16_2017.erl
+++ aoc_16_2017.erl
@@ -5,7 +5,6 @@
 
 main() ->
     Input = input(),
-    % Input = [<<"s1">>, <<"x3/4">>, <<"pe/b">>],
     Programs = << <<X>> || X <- lists:seq($a, $p)>>,
 
     Part1 = program_dance(Input, Programs),
@@ -16,21 +15,28 @@ main() ->
 
 
 program_dance_cycle(Input, Programs) ->
-    program_dance_cycle(Input, Programs, #{}, 1000000000).
+    program_dance_cycle(Input, Programs, #{}, 1000000000, 0).
 
-program_dance_cycle(_, Programs, _, 0) ->
+program_dance_cycle(_, Programs, _, 0, _) ->
     Programs;
 
-program_dance_cycle(Input, Programs, Map, Count) ->
-    {NewPrograms, NewMap} = case Map of
-        #{Programs := Val} ->
-            {Val, Map};
+program_dance_cycle(Input, Programs, Prev, Count, CurrCycleCount) ->
+    case Prev of
+        #{Programs := {PrevCycleCount, _}} ->
+            %% If we find a previous state, the answer is in the list of previous states
+            %% Calculate the remainder of the cycle length on count, this is the index
+            %% If index -1 then use the answer found in previous
+            AnswerIndex = case Count rem (CurrCycleCount-PrevCycleCount)-1 of
+                -1 -> PrevCycleCount;
+                N -> N
+            end,
+            [Result] = [ Answer || {Index, Answer} <- maps:values(Prev), Index =:= AnswerIndex],
+            Result;
         _ ->
-            Result = program_dance(Input, Programs),
-            {Result, Map#{Programs => Result}}
-    end,
-    program_dance_cycle(Input, NewPrograms, NewMap, Count-1).
-
+            NewPrograms = program_dance(Input, Programs),
+            NewPrev = Prev#{Programs => {CurrCycleCount, NewPrograms}},
+            program_dance_cycle(Input, NewPrograms, NewPrev, Count-1, CurrCycleCount+1)
+    end.
 
 program_dance([], Programs) -> Programs;
 
@@ -39,12 +45,12 @@ program_dance([<<"s", X/binary>>|Rest], Programs) ->
     program_dance(Rest, NewPrograms);
 
 program_dance([<<"x", Bin/binary>>|Rest], Programs) ->
-    [A, B] = binary:split(Bin, <<"/">>),
+    {A, B} = split_by_slash(Bin),
     NewPrograms = exchange({binary_to_integer(A), binary_to_integer(B)}, Programs),
     program_dance(Rest, NewPrograms);
 
 program_dance([<<"p", Bin/binary>>|Rest], Programs) ->
-    [A, B] = binary:split(Bin, <<"/">>),
+    {A, B} = split_by_slash(Bin),
     NewPrograms = partner({A, B}, Programs),
     program_dance(Rest, NewPrograms).
 
@@ -53,10 +59,20 @@ spin(X, Bin) ->
     <<Front:Length/binary, End/binary>> = Bin,
     <<End/binary, Front/binary>>.
 
+%% Messy duplication, but I prefer this way so you can see where the A and B values are matched
 exchange({A, B}, Bin) ->
-    ValA = binary:at(Bin, A),
-    ValB = binary:at(Bin, B),
-    partner({<<ValA>>, <<ValB>>}, Bin).
+    case A < B of
+        true -> 
+            OffsetA = A,
+            OffsetB = B-A-1,
+            <<Front:OffsetA/binary, ValA:1/binary, Mid:OffsetB/binary, ValB:1/binary, End/binary>> = Bin,
+            <<Front/binary, ValB/binary, Mid/binary, ValA/binary, End/binary>>;
+        false ->
+            OffsetB = B,
+            OffsetA = A-B-1,
+            <<Front:OffsetB/binary, ValB:1/binary, Mid:OffsetA/binary, ValA:1/binary, End/binary>> = Bin,
+            <<Front/binary, ValA/binary, Mid/binary, ValB/binary, End/binary>>
+    end.
 
 partner({A, B}, Bin) ->
     partner(A, B, Bin, <<>>).
@@ -69,13 +85,12 @@ partner(A, B, <<B:1/binary, Rest/binary>>, Acc) ->
     partner(A, B, Rest, <<Acc/binary, A/binary>>);
 partner(A, B, <<H:1/binary, Rest/binary>>, Acc) ->
     partner(A, B, Rest, <<Acc/binary, H/binary>>).
+
+split_by_slash(<<A:1/binary, "/", B:1/binary>>) -> {A, B};
+split_by_slash(<<A:1/binary, "/", B:2/binary>>) -> {A, B};
+split_by_slash(<<A:2/binary, "/", B:1/binary>>) -> {A, B};
+split_by_slash(<<A:2/binary, "/", B:2/binary>>) -> {A, B}.
     
-
-
-
-
-
-
 input() ->
     {ok, Data} = file:read_file("day16_data.txt"),
     binary:split(Data, [<<",">>, <<"\n">>], [global, trim_all]).
