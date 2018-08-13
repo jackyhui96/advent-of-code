b2ae329571e6052bd561a3f97f15048da33e782e
Jacky Hui <halojax@hotmail.com>
2018-08-13 16:33:51 +0100

    optimisations and added timings.

--- 2017/erlang/aoc_24_2017.erl
+++ 2017/erlang/aoc_24_2017.erl
@@ -3,103 +3,44 @@
 
 main() ->
     Input = input(),
-
-    %% Slow implementation, not sure how to improve
-    Bridges = find_bridges(Input),
-    {Part1, _} = lists:max(Bridges),
-
-    F = fun({Strength, Length}, {MaxStrength, MaxLength}) ->
-        case Length of
-            MaxLength ->
-                {max(Strength, MaxStrength), MaxLength};
-            L when L > MaxLength ->
-                {Strength, Length};
-            _ ->
-                {MaxStrength, MaxLength}
-        end
-    end,
-    {Part2, _} = lists:foldl(F, {0,0}, Bridges),
-
+    Part1 = part1(0, Input, 0),
+    {_Len, Part2} = part2(0, Input, {0,0}),
     {Part1, Part2}.
 
-find_bridges(List) ->
-    StartList = find_matching_parts(0, List),
-    find_bridges(StartList, List, []).
+part1(X, Pairs, Value) ->
+    Combinations = get_bridge_combinations(X, Pairs),
+    case Combinations of
+        [] -> Value;
+        _ ->
+        lists:max([begin
+            [NextX] = Pair -- [X],
+            part1(NextX, Next, Value+lists:sum(Pair))
+        end || {Pair, Next} <- Combinations])
+    end.
+ 
+part2(X, Pairs, Value={Len,Strength}) ->
+    Combinations = get_bridge_combinations(X, Pairs),
+    case Combinations of
+        [] -> Value;
+        _ ->
+            lists:max([begin
+                [NextX] = Pair -- [X],
+                part2(NextX, Next, {Len+1, Strength+lists:sum(Pair)})
+            end || {Pair, Next} <- Combinations])
+    end.
 
-find_bridges([], _, Acc) -> Acc;
-
-find_bridges([Bridge|Rest], List, Acc) when not is_list(Bridge) ->
-    NewList = List -- [Bridge],
-    Parts = find_matching_parts(Bridge, NewList),
-    NewBridges = [ [P|[Bridge]] || P <- Parts],
-    BridgeStrength = bridge_strength([Bridge]),
-    find_bridges(Rest++NewBridges, List, [{BridgeStrength, 1}|Acc]);
-
-find_bridges([Bridge|Rest], List, Acc) ->
-    NewList = List -- Bridge,
-    Parts = find_matching_parts(Bridge, NewList),
-    NewBridges = [ [P|Bridge] || P <- Parts],
-    BridgeStrength = bridge_strength(Bridge),
-    find_bridges(Rest++NewBridges, List, [{BridgeStrength, length(Bridge)}|Acc]).
-
-bridge_strength(Bridge) ->
-    lists:sum([X+Y || {X, Y} <- Bridge]).
-
-find_matching_parts(0, List) ->
-    [{0, X} || {0, X} <- List];
-find_matching_parts({0, A}, List) ->
-    F = fun(Y) ->
-        case Y of
-            {A, _} -> true;
-            {_, A} -> true;
-            _ -> false
-        end
-    end,
-    [ X || X <- List, F(X)];
-find_matching_parts([{A, B},{_, B}|_], List) ->
-    F = fun(Y) ->
-        case Y of
-            {A, _} -> true;
-            {_, A} -> true;
-            _ -> false
-        end
-    end,
-    [X || X <- List, F(X)];
-find_matching_parts([{A, B},{B, _}|_], List) ->
-    F = fun(Y) ->
-        case Y of
-            {A, _} -> true;
-            {_, A} -> true;
-            _ -> false
-        end
-    end,
-    [X || X <- List, F(X)];
-find_matching_parts([{A, B},{A, _}|_], List) ->
-    F = fun(Y) ->
-        case Y of
-            {B, _} -> true;
-            {_, B} -> true;
-            _ -> false
-        end
-    end,
-    [X || X <- List, F(X)];
-find_matching_parts([{A, B},{_, A}|_], List) ->
-    F = fun(Y) ->
-        case Y of
-            {B, _} -> true;
-            {_, B} -> true;
-            _ -> false
-        end
-    end,
-    [X || X <- List, F(X)].
+get_bridge_combinations(X, Pairs) ->
+    {Match, NoMatch} = lists:partition(fun([A,B]) -> A == X orelse B == X end, Pairs),
+    [{Pair, (Match--[Pair])++NoMatch} || Pair <- Match].
 
 input() ->
     {ok, Data} = file:read_file("../inputs/day24_data.txt"),
     Lines = binary:split(Data, <<"\n">>, [global, trim_all]),
     F = fun 
-        F(<<A:1/binary, "/", B/binary>>) ->
-            {binary_to_integer(A), binary_to_integer(B)};
-        F(<<A:2/binary, "/", B/binary>>) ->
-            {binary_to_integer(A), binary_to_integer(B)}
+        (<<A:1/binary, "/", B/binary>>) ->
+            [binary_to_integer(A), binary_to_integer(B)];
+        (<<A:2/binary, "/", B/binary>>) ->
+            [binary_to_integer(A), binary_to_integer(B)]
     end,
     [F(L) || L <- Lines].
+
--- 2017/erlang/aoc_5_2017.erl
+++ 2017/erlang/aoc_5_2017.erl
@@ -1,39 +1,73 @@
 -module(aoc_5_2017).
 -compile(export_all).
 
-main() ->
-    Input = input(),
-    NumberedInput = lists:zip(lists:seq(1, length(Input)), Input),
-    Map = maps:from_list(NumberedInput),
+% main() ->
+%     Input = input(),
+%     NumberedInput = lists:zip(lists:seq(1, length(Input)), Input),
+%     Map = maps:from_list(NumberedInput),
     
-    Part1 = map_jump(1, 0, Map),
-    Part2 = map_jump2(1, 0, Map),
+%     Part1 = map_jump(1, 0, Map),
+%     Part2 = map_jump2(1, 0, Map),
+%     {Part1, Part2}.
+
+% map_jump(Index, Count, Jumps) ->
+%     case Jumps of
+%         #{Index := Jump} ->
+%             NewJumps = Jumps#{Index := Jump+1},
+%             map_jump(Index+Jump, Count+1, NewJumps);
+%         _ -> 
+%             Count
+%     end.
+
+% map_jump2(Index, Count, Jumps) ->
+%     case Jumps of
+%         #{Index := Jump} ->
+%             NewJumps = case Jump >= 3 of
+%                 true ->
+%                     Jumps#{Index := Jump-1};
+%                 false ->
+%                     Jumps#{Index := Jump+1}
+%             end,
+%             map_jump2(Index+Jump, Count+1, NewJumps);
+%         _ -> 
+%             Count
+%     end.
+
+% input() ->
+%     {ok, Data} = file:read_file("../inputs/day5_data.txt"),
+%     Bins = binary:split(Data, <<"\n">>, [global, trim_all]),
+%     [binary_to_integer(B) || B <- Bins].
+
+main() ->
+    Offsets = input(),
+    Part1 = zip([], Offsets, 0),
+    Part2 = zip2([], Offsets, 0, fun part2_incr_func/1),
     {Part1, Part2}.
-
-map_jump(Index, Count, Jumps) ->
-    case Jumps of
-        #{Index := Jump} ->
-            NewJumps = Jumps#{Index := Jump+1},
-            map_jump(Index+Jump, Count+1, NewJumps);
-        _ -> 
-            Count
-    end.
-
-map_jump2(Index, Count, Jumps) ->
-    case Jumps of
-        #{Index := Jump} ->
-            NewJumps = case Jump >= 3 of
-                true ->
-                    Jumps#{Index := Jump-1};
-                false ->
-                    Jumps#{Index := Jump+1}
-            end,
-            map_jump2(Index+Jump, Count+1, NewJumps);
-        _ -> 
-            Count
-    end.
 
 input() ->
     {ok, Data} = file:read_file("../inputs/day5_data.txt"),
-    Bins = binary:split(Data, <<"\n">>, [global, trim_all]),
-    [binary_to_integer(B) || B <- Bins].
+    [binary_to_integer(N) || N <- binary:split(Data, <<"\n">>, [global, trim_all])].
+ 
+%% no instructions, we're done
+zip(_, [], Count) -> Count;
+zip(Prev, [N|Next], Count) when N >= 0 ->
+    {Skipped, NewNext} = take(N, {Prev, [N+1|Next]}),
+    zip(Skipped, NewNext, Count+1);
+zip(Prev, [N|Next], Count) when N < 0 ->
+    {NewNext, Skipped} = take(abs(N), {[N+1|Next], Prev}),
+    zip(Skipped, NewNext, Count+1).
+ 
+take(0, Acc) -> Acc;
+take(_, {_, []}=Acc) -> Acc;
+take(N, {Prev, [H|T]}) -> take(N-1, {[H|Prev],T}).
+ 
+zip2(_, [], Count, _) -> Count;
+zip2(Prev, [N|Next], Count, IncrFunc) when N >= 0 ->
+    {Skipped, NewNext} = take(N, {Prev, [IncrFunc(N)|Next]}),
+    zip2(Skipped, NewNext, Count+1, IncrFunc);
+zip2(Prev, [N|Next], Count, IncrFunc) when N < 0 ->
+    {NewNext, Skipped} = take(abs(N), {[IncrFunc(N)|Next], Prev}),
+    zip2(Skipped, NewNext, Count+1, IncrFunc).
+ 
+part2_incr_func(N) when N >= 3 -> N-1;
+part2_incr_func(N) -> N+1.
